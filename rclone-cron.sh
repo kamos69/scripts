c2e90c12bd8d2c5727efa3b11b123e4672d00e75
kamos69 <sikeha@gmail.com>
2017-02-28 14:48:16 +0100

    cleaned up the script a little

--- rclone-cron.sh
+++ rclone-cron.sh
@@ -3,9 +3,9 @@ if pidof -o %PPID -x "rclone-cron.sh"; then
 echo "rclone-cron.sh running"
 exit 1
 fi
-if find /home/kamos/.media/* -type f -mmin +30 | read
+if find /home/$USER/.media/* -type f -mmin +30 | read
 then
-/usr/bin/rclone copy /home/kamos/.media/ acdcrypt: --transfers 20 --checkers 20 --min-age 30 --log-file=/home/kamos/logs/local2acd.log
+/usr/bin/rclone copy /home/$USER/.media/ acdcrypt: --transfers 20 --checkers 20 --min-age 30 --log-file=/home/$USER/logs/local2acd.log
 fi
 exit
 
