fea115e0319909550bb4ccd29abb640c4af44100
kamos69 <sikeha@gmail.com>
2017-02-28 14:43:50 +0100

    cleaned up the script a little

--- acd.sh
+++ acd.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-/usr/bin/rclone mount acd:Encrypted /home/kamos/acd/.cloud --read-only --allow-other &
-sudo ENCFS6_CONFIG='/home/kamos/acd/encfs6.xml' encfs --extpass='echo ' --public /home/kamos/acd/.cloud /home/kamos/acd/cloud
-#unionfs-fuse -o cow -o allow_other /home/kamos/acd/local=RW:/home/kamos/acd/cloud=RO /home/kamos/cloud
-/usr/bin/rclone mount acdcrypt: /home/kamos/media --read-only --allow-other &
-unionfs-fuse -o cow -o allow_other /home/kamos/.media=RW:/home/kamos/media=RO /home/kamos/cloud
+/usr/bin/rclone mount acd:Encrypted /home/$USER/acd/.cloud --read-only --allow-other &
+sudo ENCFS6_CONFIG='/home/$USER/acd/encfs6.xml' encfs --extpass='echo ' --public /home/$USER/acd/.cloud /home/$USER/acd/cloud
+/usr/bin/rclone mount acdcrypt: /home/$USER/media --read-only --allow-other &
+unionfs-fuse -o cow -o allow_other /home/$USER/.media=RW:/home/$USER/media=RO /home/$USER/cloud
