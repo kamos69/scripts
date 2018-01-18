2cd24a918bc16d41207027055129043131d2fec0
kamos69 <sikeha@gmail.com>
2018-01-18 21:30:48 +0100

    Initial Commit

--- backups.sh
+++ backups.sh
@@ -5,6 +5,7 @@ echo "Starting Backup on $(date)"
 BK_PATH=/home/$USER/backups
 
 ### Sickrage
+echo "=====Sickrage====="
 # File location
 SR_PATH=/home/$USER/.sickrage
 # Create dir if it doesn't exist
@@ -13,6 +14,7 @@ mkdir -p "$BK_PATH/sickrage"
 /bin/tar -cvf $BK_PATH/sickrage/sickrage-`date \+%Y\%m\%d`.tar.gz "$SR_PATH/config.ini" "$SR_PATH/cache.db" "$SR_PATH/sickbeard.db" "$SR_PATH/cache" "$SR_PATH/failed.db"
 
 ### CouchPotato
+echo "=====CouchPotato====="
 # File location
 CP_PATH=/home/$USER/.couchpotato
 # Create dir if it doesn't exist
@@ -21,6 +23,7 @@ mkdir -p "$BK_PATH/couchpotato"
 /bin/tar -cvf $BK_PATH/couchpotato/couchpotato-`date \+%Y\%m\%d`.tar.gz "$CP_PATH/settings.conf" "$CP_PATH/database"
 
 ### Rtorrent
+echo "=====Rtorrent====="
 # File location
 RT_PATH=/home/$USER
 # Create dir if it doesn't exist
@@ -29,6 +32,7 @@ mkdir -p "$BK_PATH/rtorrent"
 /bin/tar -cvf $BK_PATH/rtorrent/rtorrent-`date \+%Y\%m\%d`.tar.gz "$RT_PATH/.sessions/" "$RT_PATH/.rtorrent.rc"
 
 ### Deluge
+echo "=====Deluge====="
 # File location
 DE_PATH=/home/$USER/.config/deluge
 # Create dir if it doesn't exist
@@ -37,6 +41,7 @@ mkdir -p "$BK_PATH/deluge"
 /bin/tar -cvf $BK_PATH/deluge/deluge-`date \+%Y\%m\%d`.tar.gz "$DE_PATH/core.conf" "$DE_PATH/web.conf" "$DE_PATH/session.state" "$DE_PATH/state/"
 
 ### PlexPy
+echo "=====PlexPy====="
 # File location
 PP_PATH=/opt/plexpy
 # Create dir if it doesn't exist
@@ -45,6 +50,7 @@ mkdir -p "$BK_PATH/plexpy"
 sudo /bin/tar -cvf $BK_PATH/plexpy/plexpy-`date \+%Y\%m\%d`.tar.gz "$PP_PATH/config.ini" "$PP_PATH/plexpy.db"
 
 ### Ombi
+echo "=====Ombi====="
 # File location
 OMBI_PATH=/opt/plexrequests
 # Create dir if it doesn't exist
@@ -53,9 +59,11 @@ mkdir -p "$BK_PATH/ombi"
 sudo /bin/tar -cvf $BK_PATH/ombi/ombi-`date \+%Y\%m\%d`.tar.gz "$OMBI_PATH/Ombi.sqlite"
 
 ### Crontabs
+echo "=====Crontabs====="
 crontab -l > $BK_PATH/cron-backup.txt
 
 ### Scripts
+echo "=====Scripts====="
 # File location
 SCRIPTS_PATH=/home/$USER/scripts
 # Create dir if it doesn't exist
@@ -64,29 +72,27 @@ mkdir -p "$BK_PATH/plexpy"
 /bin/tar -cvf $BK_PATH/scripts/scripts-`date \+%Y\%m\%d`.tar.gz $SCRIPTS_PATH
 
 ### Plex
+#echo "=====Plex====="
 # File location
-# PLEX_PATH=/var/lib/plexmediaserver/
-# Rsync to backup folder
-# sudo rsync -azv $PLEX_PATH $BK_PATH/plex/
+#PLEX_PATH=/var/lib/plexmediaserver/Library
+#Tar to backup folder
+#sudo /bin/tar -cvf $BK_PATH/plex/plexbackup-`date \+%Y\%m\%d`.tar.gz $PLEX_PATH
 
 ### Jackett
+echo "=====Jackett====="
 # File location
 JA_PATH=/home/$USER/.config/Jackett
 mkdir -p "$BK_PATH/jackett"
 # Create archive
 /bin/tar -cvf $BK_PATH/jackett/jackett-`date \+%Y\%m\%d`.tar.gz "$JA_PATH/ServerConfig.json"
 
-### Jackett
-# File location
-JA_PATH=/home/$USER/.config/Jackett
-# Create archive
-/bin/tar -cvf $BK_PATH/jackett/jackett-`date \+%Y\%m\%d`.tar.gz "$JA_PATH/ServerConfig.json"
-
 ### Rclone
+echo "=====Rclone====="
 # Rsync to backup folder
 sudo rsync -azv /home/$USER/.rclone.conf $BK_PATH/
 
 ### Config
+echo "=====Config Folder====="
 # File location
 CONFIG_PATH=/home/$USER/.config/
 mkdir -p "$BK_PATH/config"
@@ -94,6 +100,7 @@ mkdir -p "$BK_PATH/config"
 sudo /bin/tar -cvf $BK_PATH/config/config-`date \+%Y\%m\%d`.tar.gz  $CONFIG_PATH
 
 ### Sync to Cloud
+echo "=====Sync to Cloud====="
 # File Location
 ACD_PATH=backups:
 # Rclone sync to ACD
@@ -101,4 +108,4 @@ ACD_PATH=backups:
 /usr/bin/rclone copy --transfers=10 --checkers=10 $BK_PATH/ $ACD_PATH -v
 
 ### Clean up old backups
-/usr/bin/rclone delete $BK_PATH/ --min-age 1M
+#/usr/bin/rclone delete $BK_PATH/ --min-age 1M
