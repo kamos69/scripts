d3c4c4adce576b103139578b363f6aa3bd83bfef
kamos69 <sikeha@gmail.com>
2018-01-24 00:20:14 +0100

    update

--- rclone-cron.sh
+++ rclone-cron.sh
@@ -5,7 +5,7 @@ exit 1
 fi
 if find /home/kamos/.media/* -type f -mmin +15 | read
 then
-/usr/bin/rclone copy /home/kamos/.media/ boxee: --transfers 20 --checkers 20 --min-age 15 --stats 10s --bwlimit 8M --exclude "/.unionfs-fuse/**" --log-file=/home/kamos/logs/local2gdrive.log -vv
+/usr/bin/rclone copy /home/kamos/.media/ boxee: --transfers 20 --checkers 20 --min-age 15 --bwlimit 8M --exclude "/.unionfs-fuse/**" --log-file=/home/kamos/logs/local2gdrive.log
 fi
 exit
 
