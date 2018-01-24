08745e5ab73ff920393b9b7ce55c6489bf114041
kamos69 <sikeha@gmail.com>
2018-01-24 18:58:06 +0100

    Update

--- checkdisk.sh
+++ checkdisk.sh
@@ -21,7 +21,9 @@ if [ ${current_usage%?} -ge ${max_usage%?} ]; then
     echo "$(date "+%d.%m.%Y %T") Current disk space is above threshold. Deleting files older than $max_age days." | tee -a "$LOGFILE"
     find $media_folder -type f -mtime +$file_age -delete
     current_usage=$( df -h | grep $root | awk {'print $5'} )
-    echo "$(date "+%d.%m.%Y %T") Disk Cleanup complete. Current disk use is $current_usage." | tee -a "$LOGFILE"
+    echo "$(date "+%d.%m.%Y %T") Disk check done. Current disk use is $current_usage." | tee -a "$LOGFILE"
+    echo "$(date "+%d.%m.%Y %T") Cleaning up..." | tee -a "$LOGFILE"
+    find -type d -empty -delete
 
 elif [ ${current_usage%?} -lt ${max_usage%?} ]; then
     echo "$(date "+%d.%m.%Y %T") Current disk space is below threshold." | tee -a "$LOGFILE"
--- rclone-cron.sh
+++ rclone-cron.sh
@@ -5,7 +5,11 @@ exit 1
 fi
 if find /home/kamos/.media/* -type f -mmin +15 | read
 then
-/usr/bin/rclone copy /home/kamos/.media/ boxee: --transfers 20 --checkers 20 --min-age 15 --bwlimit 8M --exclude "/.unionfs-fuse/**" --log-file=/home/kamos/logs/local2gdrive.log
+start=$(date +'%s')
+echo "$(date "+%d.%m.%Y %T") RCLONE UPLOAD STARTED"
+# MOVE FILES OLDER THAN 15 MINUTES
+/usr/bin/rclone copy /home/kamos/.media/ boxee: --transfers 20 --checkers 20 --min-age 15 --bwlimit 8M --exclude "/.unionfs-fuse/**" --log-file=/home/kamos/logs/local2gdrive.log -v
+echo "$(date "+%d.%m.%Y %T") RCLONE UPLOAD FINISHED IN $(($(date +'%s') - $start)) SECONDS"
 fi
 exit
 
