c0547c23c6dbe82652b26a1404c11a643474bfd3
kamos69 <sikeha@gmail.com>
2018-04-15 13:13:32 +0200

    Update

--- rclone-cron.sh
+++ rclone-cron.sh
@@ -13,7 +13,7 @@ start=$(date +'%s')
 echo "$(date "+%d.%m.%Y %T") RCLONE UPLOAD STARTED" | tee -a $LOGFILE
 
 # MOVE FILES OLDER THAN 15 MINUTES
-/usr/bin/rclone copy "$FROM" "$TO" --transfers=20 --checkers=20 --exclude "/.unionfs-fuse/**" --min-age 15m --bwlimit 8M --log-file=$LOGFILE -v
+/usr/bin/rclone copy "$FROM" "$TO" --transfers=20 --checkers=20 --exclude "/.unionfs-fuse/**" --min-age 15m --bwlimit 8M --log-file=$LOGFILE --log-level INFO
 echo "$(date "+%d.%m.%Y %T") RCLONE UPLOAD FINISHED IN $(($(date +'%s') - $start)) SECONDS" | tee -a $LOGFILE
 fi
 exit
