9c4272f07a6bb44cb6967383e2a3f20d8e8d29a9
kamos69 <sikeha@gmail.com>
2018-09-21 13:43:42 +0200

    initial commit

---
+++ script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#name=Plex Media Server check and restart
+#description=This script will restart PLEX if it does not respond after two attempts.
+#arrayStarted=true
+
+
+dockerid=$(docker ps -aqf "name=plex")
+if [ "$dockerid" == "" ]; then
+	echo "ERR $(date -Is) - Could not get a docker id for docker name \"Plex\"."
+	exit 1;
+fi
+
+# Do not check between 1:55am to 2:30am
+currentTime=`date +"%H%M%S"`
+if [[ ! ( "$currentTime" < "015500" || "$currentTime" > "023000" ) ]]; then
+	exit 0;
+fi
+
+firstcheck=$((curl -sSf -m30 http://127.0.0.1:32400/web/) 2>&1)
+if [ "$firstcheck" != "" ]; then
+	echo "WRN $(date -Is) - Plex did not respond in first check, waiting 15 seconds.."
+	sleep 15
+	secondcheck=$((curl -sSf -m30 http://127.0.0.1:32400/web/) 2>&1)
+	if [ "$secondcheck" != "" ]; then
+		echo "WRN $(date -Is) - Plex did not respond in second check either, restarting docker container."
+
+		echo "INF $(date -Is) - Stopping docker $dockerid."
+#		docker stop $dockerid
+
+		echo "INF $(date -Is) - Waiting 15 seconds.."
+		sleep 15
+
+		echo "INF $(date -Is) - Starting docker $dockerid."
+#		docker start $dockerid
+	else
+		echo "INF $(date) - Plex docker container responded on second attempt."
+	fi
+else
+	echo "INF $(date) - Plex docker container responded on first attempt."
+	source pushbullet.sh "Boxee" "INF $(date) - Plex docker container responded on first attempt."
+fi
